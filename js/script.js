b6fe417d698a2089fd532da25f514d62db04602e
alberto <alberto.taddeucci@gmail.com>
2024-02-20 14:20:23 +0100

    milestone 1 completata

--- js/script.js
+++ js/script.js
@@ -6,7 +6,28 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            tasks:[
+                {
+                    text: "Fare la spesa",
+                    done: true
+                },
+                {
+                    text: "Chiamare il meccanico",
+                    done: false
+                },
+                {
+                    text: "Andare in palestra",
+                    done: false
+                },
+                {
+                    text: "Fare la lavatrice",
+                    done: true
+                },
+                {
+                    text: "Aggiustare il comodino",
+                    done: false
+                },
+            ]
             
             
             
